12f649fed20ad1591c29709d47dfeb1cce35d617
Archimedyz <awais.ali1248@gmail.com>
2021-11-02 19:11:14 -0400

    Complete 2015/day-4.

---
+++ 2015/day-4/problem-1/main.js
@@ -0,0 +1,19 @@
+const md5 = require('md5');
+
+var zeroCount = 5;
+var prefix = '0'.repeat(zeroCount);
+var input = "iwrupvqb";
+
+var limit = 10000000;
+
+
+for (var i = 1; i < limit; ++i)
+{
+    var hash = md5(input + i);
+    if (hash.startsWith(prefix)) {
+        console.log('OUTPUT >> ' + i);
+        return;
+    }
+}
+
+console.error('ERROR >> Did not find valid hash within limit ' + limit);
